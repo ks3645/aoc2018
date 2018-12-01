2b3e09679f66333a409234a6fdecb058af24eacc
Karl Schober <ks3645@gmail.com>
2018-11-30 22:40:19 -0600

    initial commit

--- aoc2018/src/main.rs
+++ aoc2018/src/main.rs
@@ -1,3 +1,22 @@
+macro_rules! days {
+    ( $( $x:ident ),* ) => {
+        $(
+            mod $x;
+        )*
+        fn print_all_solutions() {
+            $(
+                  println!("{}: Part One Solution: {}", stringify!($x), $x::solve(Part::PartOne));
+                  println!("{}: Part Two Solution: {}", stringify!($x), $x::solve(Part::PartTwo));
+            )*
+        }
+    };
+}
+
+mod utils;
+use utils::Part;
+days!(day1);
+
+
 fn main() {
-    println!("Hello, world!");
+    print_all_solutions();
 }
