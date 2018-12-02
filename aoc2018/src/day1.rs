c0f7750a1aaff1fa6618380f1d9a8da3486ef3a5
Karl Schober <ks3645@gmail.com>
2018-12-01 18:52:08 -0600

    some day 1 cleanup

--- aoc2018/src/day1.rs
+++ aoc2018/src/day1.rs
@@ -6,51 +6,40 @@ pub fn solve(part: Part) -> i32 {
     let mut input = String::new();
     utils::read_input_to_string(&mut input, 1).unwrap();
 
-    do_the_thing(input, part)
+    find_frequency(input, part)
 }
 
-pub fn do_the_thing(input:String, part: Part) -> i32 {
-    let changes:Vec<String> =  input.trim().split('\n').map(|s| String::from(s)).collect();
+pub fn find_frequency(input:String, part: Part) -> i32 {
+    let changes =  input.trim().split('\n')
+        .map(|s| String::from(s))
+        .map(|s| s.trim().parse::<i32>().unwrap());
 
-    let mut result_set = HashSet::new();
+    let mut seen_freqs = HashSet::new();
 
-    let mut result = 0;
-
-    let mut freq_changes = changes.clone().into_iter();
-    let mut freq_cycle = changes.into_iter().cycle();
-
-    'main: loop {
-        let change;
-
-        match part {
-            Part::One => {
-                match freq_changes.next() {
-                    Some(thing) => change = thing,
-                    None => break 'main,
-                }
-            },
-            Part::Two => {
-                match freq_cycle.next() {
-                    Some(thing) => change = thing,
-                    None => break 'main,
-                }
-            }
-        }
-
-        result += change.trim().parse::<i32>().unwrap();
-
-        match part {
-            Part::One => {},
-            Part::Two => {
-                if result_set.contains(&result) {
-                    break 'main;
-                } else {
-                    result_set.insert(result);
+    let result:i32 = match part {
+        Part::One => changes.sum(),
+        Part::Two => {
+            let mut cycle = changes.cycle();
+            let mut freq = 0;
+            let mut first_repeat= i32::min_value();
+            'change_cycle: loop {
+                match cycle.next() {
+                    Some(change) => {
+                        freq += change;
+                        if seen_freqs.contains(&freq) {
+                            first_repeat = freq;
+                            break 'change_cycle;
+                        }
+                        else {
+                            seen_freqs.insert(freq);
+                        }
+                    },
+                    None => break 'change_cycle,
                 }
             }
+            first_repeat
         }
-
-    }
+    };
 
     result
 }
